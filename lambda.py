914606fe6e92ec497c88a1ef7bf24a744ad981be
johnbutler123 <10353776@mydbs.ie>
2017-05-19 10:01:33 +0100

    updated ca5calc.py for assessment

--- lambda.py
+++ lambda.py
@@ -1,83 +1,88 @@
 add = lambda x,y: x+y
 
-print add(1,1)
+print "answer to lambda add = ", add(1,1)
 
 def fahrenheit(t):
     return ((float(9)/5)*t + 32)
 def celsius(t):
     return (float(5)/9*(t - 32))
-temp = (36.5, 37, 37.5, 39)
+temp = (36.5, 37, 37.5, 39,10)
 
 F = map(fahrenheit, temp)
-print F
+print "Temperature Farenheit is ",  F
 C = map(celsius, F)
-print C
+print "Temperature Celcius is ", C
 
 a = [1,2,3,4]
 b = [17,12,11,10]
 c = [-1, -4, 5, 9]
-print map(lambda x,y:x+y, a,b)
-print map(lambda x,y,z:x+y+z, a,b,c)
-print map(lambda x,y,z:x+y-z, a,b,c)
+print "'a' list  + 'b' list = ", map(lambda x,y:x+y, a,b)
+print "'a' list + 'b' list + 'c' list = ", map(lambda x,y,z:x+y+z, a,b,c)
+print "a' list + 'b' list - 'c' list = ", map(lambda x,y,z:x+y-z, a,b,c)
 
 fib = [0,1,1,2,3,5,8,13,21,34,55]
 result = filter(lambda x: x % 2, fib)
-print result
+print "list as filtered to show odd numbers only", result
+
+
+
 
 result = filter(lambda x: x % 2 == 0, fib)
-print result
+print "list as filtered to show even numbers only", result
 
-print reduce(lambda x, y: x+y, [47, 11, 42, 13])
+print "sum items in list ", reduce(lambda x, y: x+y, [47, 11, 42, 13])
 
 max = lambda a,b: a if (a>b) else b
-print reduce(max, [47, 11, 42, 13]) 
+print " max of numbers in list = ", reduce(max, [47, 11, 42, 13]) 
 
 def max(values):
 	return reduce(lambda a,b: a if (a>b) else b, values)
 	
-print max([47, 11, 42, 13])	
+print "Maximum of numbers in list = ", max([47, 11, 42, 13])	
 
 def min(values):
 	return reduce(lambda a,b: a if (a<b) else b, values)
 	
-print min([47, 11, 42, 13])	
+print "Minimum of numbers in list = ", min([47, 11, 42, 13])	
 
 def add(values):
 	return reduce(lambda a,b: a+b, values)
 	
-print add([47, 11, 42, 13])	
+print "sum of numbers in list = ", add([47, 11, 42, 13])	
 
 def sub(values):
 	return reduce(lambda a,b: a-b, values)
 	
-print sub([47, 11, 42, 13])	
+print "cumulative total of subtract consequetive numbers in list ", sub([47, 11, 42, 13])	
 
 def mul(values):
 	return reduce(lambda a,b: a*b, values)
 	
-print mul([47, 11, 42, 13])	
+print "Product of items in list = ", mul([3, 5, 2, 4])	
 
 def div(values):
 	return reduce(lambda a,b: a/float(b) if (b != 0 and a != 'Nan') else 'Nan', values)
 	
-print div([47, 'Nan', 0, 11])
+print "Result of dividing by consequetive numbers in list ",  div([100, 2, 2, 5])
 
 def is_even(values):
 	return filter(lambda x: x % 2 == 0, values)
 	
-print is_even([47, 11, 42, 13])	
+print "Show even numbers in list ", is_even([47, 12, 42, 13])	
 
 def to_fahrenheit(values):
 	return map(fahrenheit, values)
 	
-print to_fahrenheit([0, 37, 40, 100])	
+print "Conversion to Farenheit for items in list is ", to_fahrenheit([0, 37, 40, 100])	
 	
 def to_celsius(values):
 	return map(celsius, values)	
 	
-print to_celsius([0, 32, 100, 212])
+print "Conversion to Celcius for items in list is ", to_celsius([0, 32, 100, 212])
 
 def sum(to):
 	return reduce(lambda x, y: x+y, range(1, to+1))
 	
-print sum(100)
+print "Cumulative total of numbers in range = ", sum(100)
+
+
--- list_comp.py
+++ list_comp.py
@@ -2,12 +2,13 @@
 
 Celsius = [39.2, 36.5, 37.3, 37.8]
 Fahrenheit = [ ((float(9)/5)*x + 32) for x in Celsius ]
-print Fahrenheit
+print "Farenheit temperature is ", Fahrenheit
 
+n = 30
+print "list of pythagorean triples"
+print [(x,y,z) for x in range(1,n) for y in range(x,n) for z in range(y,n) if x**2 + y**2 == z**2]
 
-print [(x,y,z) for x in range(1,30) for y in range(x,30) for z in range(y,30) if x**2 + y**2 == z**2]
-
-
+print "alternative coding for pythagorean triples"
 pyt_triples = []
 for x in range(1, 30):
 	for y in range(x,30):
@@ -23,7 +24,7 @@ print pyt_triples
 #	my_list.append(((float(9)/5)*x + 32) )
 	
 
-
+print "Cartesian join of 'colours' and 'things'"
 colours = [ "red", "green", "yellow", "blue" ]
 things = [ "house", "car", "tree" ]
 coloured_things = [ (x,y) for x in colours for y in things ]
